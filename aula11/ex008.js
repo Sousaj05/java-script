720f01bd553557e19382f0dce811f1d6ff9949b0
Sousaj05 <araujojordao06@gmail.com>
2024-10-17 21:46:58 -0300

    condicao

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro e ${vel}Km/hr`)
+if (vel > 60) {
+    console.log('Voce ultrapassou o limite de velociade. MUTADO!')
+}
+console.log('Dirija sempre usando o cinto de seguranca!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Suica'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log(' Voce e Brasileiro')
+} else {
+    console.log('Voce e Estrangeiro')
+}
