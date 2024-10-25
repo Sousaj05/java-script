f9723a152aa4801af7786c9c9c1db35ea08d9c8e
Sousaj05 <araujojordao06@gmail.com>
2024-10-24 21:19:06 -0300

    aula sobre repeticao finalizada

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1      /* Teste logico no final */
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 99)
+
+
+/* Teste logico no inico
+var c = 1
+while (c <= 99) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ aulo14/ambiente.js
@@ -0,0 +1,14 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*---------------------------------*/
+console.log('Vai comecar...')
+for (var c=1 ; c <=4 ; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
