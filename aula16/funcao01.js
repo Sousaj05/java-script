04d8f9847014de60252429d7fecf7bbaa217827c
Sousaj05 <araujojordao06@gmail.com>
2024-10-31 21:15:37 -0300

    funcoes

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(10))
+
+/* Maneira com variavel
+var res = parimpar(95) 
+    console.log(res)
+*/
