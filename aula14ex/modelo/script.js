97b31693fa7c4f8df74bda3d5c02875e76ae8a4a
Sousaj05 <araujojordao06@gmail.com>
2024-12-11 20:49:33 -0300

    fim do primeiro ex aula 14

--- aula14ex/modelo/script.js
+++ aula14ex/modelo/script.js
@@ -1,31 +1,29 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-    if(ini.value.lenght <= 0 || fim.value.lenght <= 0 || passo.value.value <= 0) {
-      window.alert("Impossivel contar")
-    } else {
-      res.innerHTML = 'Contando; <br>'
-      let i = Number(ini.value)
-      let f = Number(fim.value)
-      let p = Number(passo.value)
-      if(p <= 0) {
-        window.alert('Passo invalido! Considerado PASSO 1')
-        p = 1
+  let ini = document.getElementById('txti')
+  let fim = document.getElementById("txtf")
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+  if(ini.value.lenght <= 0 || fim.value.lenght <= 0 || passo.value.lenght <= 0) {
+    res.innerHTML = "impossivel contar"
+  } else {
+    res.innerHTML = 'Contando... <br>'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if(p <= 0) {
+      window.alert('PASSO INVALIDO! CONSIDERADO PASSO 1')
+      P = 1
+    } 
+    if(i < f) {
+      //Contagem crescente
+      for(let c = i ; c <= f ; c+=p) {
+        res.innerHTML += `${c} \u{1F449}`
       }
-      if(i < f) {
-        // Contagem crescente
-        for(let c = i; c <= f ; c+=p) {
-          res.innerHTML += `${c} \u{1F449}`
-        }
       } else {
-        // Contagem regressiva
+        //Contagem regressiva
         for(let c = i ; c >= f ; c-=p) {
           res.innerHTML += `${c} \u{1F449}`
         }
       }
-      res.innerHTML += `\u{1F3C1}`
     }
-}
+  }
