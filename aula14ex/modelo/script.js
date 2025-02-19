d456580174d563098845f63493d45a48d00ca4ec
Sousaj05 <araujojordao06@gmail.com>
2025-02-18 22:30:22 -0300

    treinando

--- aula14ex/modelo/script.js
+++ aula14ex/modelo/script.js
@@ -1,11 +1,31 @@
-function tabuada() {
-  let num = document.getElementById('txtn')
-  let tab = document.getElementById('seltab')
-  if(num.value.lenght == 0) {
-    window.alert('Por favor, Digite um numero')
-  } else {
-    let n = Number(num.value)
-    let c = 1
-    tab.innerHTML = ""
+function contar() {
+let ini = document.getElementById("txti")
+let fim = document.getElementById("txtf")
+let passo = document.getElementById("txtp")
+let res = document.getElementById("res")
+
+if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+  res.innerHTML = 'Impossivel contar!'
+} else {
+  res.innerHTML = 'Contando; <br>'
+  let i = Number(ini.value)
+  let f = Number(fim.value)
+  let p = Number(passo.value)
+  if(p <= 0) {
+    window.alert('Impossivel contar! Considerado PASSO 1')
+    p=1
   }
+  if(i < f) {
+    //Ordem crescente
+    for(let c = i ; c <= f ; c+=p){
+    res.innerHTML += `${c} \u{1F449}`
+    }
+  } else {
+    //Ordem decrescente
+    for(let c = i ; c >= f ; c-=p) {
+    res.innerHTML += `${c} \u{1F449}`
+    }
+  }
+  res.innerHTML += `\u{1F3C1}`
+}
 }
